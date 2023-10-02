dab53911b1511394dbfbf8594153e8b5c36d2bdd
Alphilus <58624681+Alphilus@users.noreply.github.com>
2023-10-02 20:51:28 +0700

    Add files via upload

---
+++ Buoi 2/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String s1 = "I ";
+        String s2 = "hate";
+        String s3 = " you";
+        System.out.println(s1 + "love" + s3 + " forever");
+        System.out.println(s1 + "love" + s3.substring(0,2));
+        System.out.println(s1 + s2 + s3);
+    }
+}
