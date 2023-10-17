d6db66c107c3018d6d0773d69d0e968473d9b1a2
Alphilus <58624681+Alphilus@users.noreply.github.com>
2023-10-17 13:50:48 +0700

    Update Main.java

--- Buoi_8/src/com/company/Main.java
+++ Buoi_8/src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -74,33 +76,79 @@ public class Main {
 
 //        QL Trận đấu xếp hạng
 
-        ArrayList<SummonRift> summonRifts = new ArrayList<>();
+//        ArrayList<SummonRift> summonRifts = new ArrayList<>();
+//
+//            int members = 5;
+//
+//            System.out.println("Nhập thông tin nhóm thứ "+(members+1));
+//            ArrayList<SKT> skt = new ArrayList<>();
+//            for (int k = 0; k < members; k++){
+//                System.out.println("Nhập tên của tướng thứ "+(k+1));
+//                String name = scanner.nextLine();
+//                System.out.println("Nhập vị trí của tướng"+(k+1));
+//                String position = scanner.nextLine();
+//                SKT skt1 = new SKT(name, position);
+//                skt.add(skt1);
+//            }
+//            System.out.println(skt);
+//
+//            System.out.println("Nhập thông tin nhóm thứ "+(members+1));
+//            ArrayList<G2> g2s = new ArrayList<>();
+//            for (int j = 0; j < members; j++){
+//                System.out.println("Nhập tên của tướng thứ "+(j+1));
+//                String name = scanner.nextLine();
+//                System.out.println("Nhập vị trí của tướng"+(j+1));
+//                String position = scanner.nextLine();
+//                G2 g2s1 = new G2(name, position);
+//                g2s.add(g2s1);
+//            }
+//            System.out.println(g2s);
+//        System.out.println(summonRifts);
 
-            int members = 5;
+//        Bài 2
+        ArrayList<String> students = new ArrayList<>(Arrays.asList("id: 593 ","Kim ","Tuổi: 20"," Học Lực:"," Yếu"));
 
-            System.out.println("Nhập thông tin nhóm thứ "+(members+1));
-            ArrayList<SKT> skt = new ArrayList<>();
-            for (int k = 0; k < members; k++){
-                System.out.println("Nhập tên của tướng thứ "+(k+1));
-                String name = scanner.nextLine();
-                System.out.println("Nhập vị trí của tướng"+(k+1));
-                String position = scanner.nextLine();
-                SKT skt1 = new SKT(name, position);
-                skt.add(skt1);
-            }
-            System.out.println(skt);
+        ArrayList<String> students2 = new ArrayList<>(Arrays.asList("id: 654 ","Lan ","Tuổi: 21"," Học Lực:"," TB"));
 
-            System.out.println("Nhập thông tin nhóm thứ "+(members+1));
-            ArrayList<G2> g2s = new ArrayList<>();
-            for (int j = 0; j < members; j++){
-                System.out.println("Nhập tên của tướng thứ "+(j+1));
-                String name = scanner.nextLine();
-                System.out.println("Nhập vị trí của tướng"+(j+1));
-                String position = scanner.nextLine();
-                G2 g2s1 = new G2(name, position);
-                g2s.add(g2s1);
-            }
-            System.out.println(g2s);
-        System.out.println(summonRifts);
+        ArrayList<String> students3 = new ArrayList<>(Arrays.asList("id: 169 ","Tân ","Tuổi: 25"," Học Lực:"," Giỏi"));
+
+        System.out.println("Cập nhật thông tin học lực của học viên id:");
+        int id = Integer.parseInt(scanner.nextLine());
+
+        if (id == 593){
+            System.out.println("Cập nhập điểm(Yếu/TB/Khá/Giỏi)");
+            String score = scanner.nextLine();
+            students.set(4,score);
+        }else  if (id == 654) {
+            System.out.println("Cập nhập điểm(Yếu/TB/Khá/Giỏi)");
+            String score = scanner.nextLine();
+            students2.set(4, score);
+        }else  if (id == 169) {
+            System.out.println("Cập nhập điểm(Yếu/TB/Khá/Giỏi)");
+            String score = scanner.nextLine();
+            students3.set(4, score);
+        }
+
+        System.out.println("Nhập id của một học sinh để xóa:");
+        id = Integer.parseInt(scanner.nextLine());
+
+        if (id == 593){
+            students.clear();
+        }else  if (id == 654) {
+            students2.clear();
+        }else  if (id == 169) {
+            students3.clear();
+        }
+
+
+        ArrayList<String> classRoom = new ArrayList<>(List.of(" Môn học: IT "+students+students2+students3));
+
+        ArrayList<String> students4 = new ArrayList<>(List.of("id: 020 ","Minh ","Tuổi: 22"," Học Lực:"," Khá"));
+
+        classRoom.add(String.valueOf(students4));
+
+        ArrayList<String> techMaster = new ArrayList<>(List.of("Quản lý lớp học: Cường "+" Giảng viên: Đức "+classRoom));
+
+        System.out.println(techMaster);
     }
 }
